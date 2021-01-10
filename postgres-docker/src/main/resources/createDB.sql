43e10df3ea27867e0d6c52656b24dfc6a63e0da6
arshads96 <arshads96@gmail.com>
2021-01-10 20:01:05 +0530

    Postgres Docker Image

---
+++ postgres-docker/src/main/resources/createDB.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE customer_management OWNER=postgres CONNECTION LIMIT = -1;
+CREATE DATABASE product_management OWNER=postgres CONNECTION LIMIT = -1;
+CREATE DATABASE invoice_management OWNER=postgres CONNECTION LIMIT = -1;
